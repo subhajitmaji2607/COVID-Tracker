7d569084d7877ebde85907666330a763f44f1b80
Subhajit Maji <subhajitmaji2607@gmail.com>
2021-12-12 01:24:28 +0530

    fix some issue

--- src/components/Navigationbar.vue
+++ src/components/Navigationbar.vue
@@ -1,11 +1,9 @@
 <template>
-    <nav>
-    <v-toolbar app>
+    <v-app-bar app>
       <v-icon x-large color="red accent-3">coronavirus</v-icon>
       <!-- <v-toolbar-title class="ml-4">Hans mat pagli, pyar ho jayega, teeka lagwa le, corona haar jayega</v-toolbar-title> -->
       <v-toolbar-title class="ml-4"><span class="red--text text--darken-1">COVID-19 </span><span>In India</span></v-toolbar-title>
-    </v-toolbar>
-    </nav>
+    </v-app-bar>
 </template>
 
 <script>
