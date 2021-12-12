384c229e7f2dd71f08a8258a7cdb88e3aeac137f
Subhajit Maji <subhajitmaji2607@gmail.com>
2021-12-12 19:12:49 +0530

    intrtgate daily confirmed chart in frontend

--- src/components/DetailsComponent.vue
+++ src/components/DetailsComponent.vue
@@ -1,29 +1,34 @@
 <template>
 <v-main class="pt-6">
-   <v-row dense>
-       <v-col cols="6" xs="12">
-           <v-card elevation="3" color="red lighten-4" style="min-height: 300px">
-               <Graph type="area" :graphColor="['#C62828']" title="Confirmed" textColor='#EF5350' />
-           </v-card>
-       </v-col>
-       <v-col cols="6" xs="12">
-           <v-card elevation="3" color="blue-grey lighten-4" style="min-height: 300px">
+    <v-row dense>
+        <v-col cols="6" xs="12">
+            <v-card elevation="3" color="red lighten-4" style="min-height: 300px">
+                <Graph
+                  type="area"
+                  :graphColor="['#C62828']"
+                  title="Daily Confirmed Cases"
+                  textColor='#EF5350'
+                />
+            </v-card>
+        </v-col>
+        <v-col cols="6" xs="12">
+            <v-card elevation="3" color="blue-grey lighten-4" style="min-height: 300px">
 
-           </v-card>
-       </v-col>
-   </v-row>
-   <v-row dense class="mt-2">
-       <v-col cols="6" xs="12">
-           <v-card elevation="3" color="green lighten-4" style="min-height: 300px">
+            </v-card>
+        </v-col>
+    </v-row>
+    <v-row dense class="mt-2">
+        <v-col cols="6" xs="12">
+            <v-card elevation="3" color="green lighten-4" style="min-height: 300px">
 
-           </v-card>
-       </v-col>
-       <v-col cols="6" xs="12">
-           <v-card elevation="3" color="grey lighten-4" style="min-height: 300px">
+            </v-card>
+        </v-col>
+        <v-col cols="6" xs="12">
+            <v-card elevation="3" color="grey lighten-4" style="min-height: 300px">
 
-           </v-card>
-       </v-col>
-   </v-row>
+            </v-card>
+        </v-col>
+    </v-row>
 </v-main>
 </template>
 
@@ -31,8 +36,32 @@
 import Graph from './graph/GraphComponent.vue'
 export default {
     name: 'DetailsComponent',
-    components:{
+    components: {
         Graph,
-    }
+    },
+    data: () => ({
+        xaxisDataForConfirmedGraph: [],
+        dataForConfirmedGraph:[],
+    }),
+    props: {
+        
+    },
+    // methods: {
+    //     filterDataForConfirmedGraph(data) {
+    //         console.log(data)
+    //     }
+    // },
+    // mounted() {
+    //     // this.filterDataForConfirmedGraph(this.data)
+    //     this.xaxisDataForConfirmedGraph = this.data
+    //     console.log('call')
+    // },
+    // watch:{
+    //     rawDataForGraph(graphData){
+    //         // console.log('data update')
+    //         this.xaxisDataForConfirmedGraph = graphData.map((data)=>data.dateymd)
+    //         this.dataForConfirmedGraph = graphData.map((data)=>data.dailyconfirmed)
+    //     }
+    // }
 }
 </script>
